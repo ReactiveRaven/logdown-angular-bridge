95017ade0fc5365d87a14fa9a34613736b296403
Elliot Wright <wright.elliot@gmail.com>
2015-08-21 11:47:58 +0100

    Added support for getting Logdown via require().

--- logdown-service.js
+++ logdown-service.js
@@ -3,8 +3,21 @@ angular.module(
     []
 )
     .service("Logdown", function() {
-        if (typeof Logdown !== "function") {
+        var logdown;
+
+        if (
+            typeof module !== "undefined" &&
+            typeof module.exports !== "undefined" &&
+            typeof require === "function"
+        ) {
+            logdown = require("logdown");
+        } else {
+            logdown = window.Logdown;
+        }
+
+        if (typeof logdown !== "function") {
             throw new Error("Logdown is not defined; did you include the logdown script?");
         }
-        return Logdown;
+
+        return logdown;
     });
